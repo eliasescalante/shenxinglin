25df71a4333ed0d09d23bbb4b47b8b06ba47ccce
eliasescalante <eliasw.escalante@gmail.com>
2023-12-30 17:31:10 -0300

    actualizacion de estructura en la web y agregue las nuevas html

--- shenxinglin/alumnos/urls.py
+++ shenxinglin/alumnos/urls.py
@@ -5,4 +5,8 @@ from alumnos import views
 
 urlpatterns = [
     path('', views.inicio ,name="Inicio"),
+    path('noticias', views.noticias, name="Noticias"),
+    path('alumnos', views.alumnos, name="Alumnos"),
+    path('staff', views.staff, name="Staff"),
+    path('escuela', views.escuela, name="Escuela"),
 ]
--- shenxinglin/alumnos/views.py
+++ shenxinglin/alumnos/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, './alumnos/index.html')
+
+def escuela(request):
+    return render(request, './alumnos/escuela.html')
+
+def noticias(request):
+    return render(request, './alumnos/noticias.html')
+
+
+def staff(request):
+    return render(request, './alumnos/staff.html')
+
+def alumnos(request):
+    return render(request, './alumnos/alumnos.html')
